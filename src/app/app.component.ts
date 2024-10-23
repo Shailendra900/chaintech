2d5b48d4e7bda0d131a59a8f31df103ef5499006
Shailendra900 <shailkumarverma900@gmail.com>
2024-10-23 19:30:56 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ApiService } from './service/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'chaintech';
+export class AppComponent implements OnInit {
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+
+
 }
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor( private http:HttpClient) { }
+
+  
+}
